767076316735d59f8d4b004e3288596807c92871
Eyasu Ashebier <amenjosh@gmail.com>
2014-08-27 03:20:47 -0700

    Just for test purpose.

---
+++ sources/Medication_Reminder/remindMe/RemindMeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Medication_Reminder.remindMe
+{
+    public class RemindMeContext
+    {
+        public RemindMeContext()
+        {
+            var test = 8;
+            var test1 = test>5? 8:9;
+        }
+    }
+}
